89de1a04d0d47ab0ede53b998b8ad8dc0b9a77da
thenolin <nateolin17@gmail.com>
2020-02-28 09:59:11 -0600

    creating coutners component to house multiple counter components with their own instance

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,21 +2,25 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
-    tags: ["tag1", "tag2", "tag3"]
+    count: 0
   };
+
+  handleIncrement = product => {
+    console.log(product);
+    this.setState({ count: this.state.count + 1});
+  }
 
   render() {
     return (
-      <React.Fragment>
+      <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
-        <ul>
-          {this.state.tags.map(tag => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
-      </React.Fragment>
+        <button 
+          onClick={product => this.handleIncrement(product)}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+      </div>
     );
   }
 
