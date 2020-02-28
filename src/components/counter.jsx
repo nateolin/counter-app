992a706de2c4e8c25ccf693a348fe799d5a2d24b
thenolin <nateolin17@gmail.com>
2020-02-28 16:18:07 -0600

    adding handleDelete arrow function to handle delete from child counter component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,22 +2,28 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    value: this.props.value
+    value: this.props.counter.value
   };
 
   handleIncrement = () => {
-    this.setState({ value: this.state.value + 1});
-  }
+    this.setState({ value: this.state.value + 1 });
+  };
 
   render() {
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button 
+        <button
           onClick={product => this.handleIncrement(product)}
           className="btn btn-secondary btn-sm"
         >
           Increment
+        </button>
+        <button
+          onClick= {() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
         </button>
       </div>
     );
